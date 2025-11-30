2020bf37b135b0276c2d6b5ccc49d145830b6009
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-30 12:54:02 -0400

    desktop entry name is cursed

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/kronos.png
 export DESKTOP=/usr/share/applications/kronos.desktop
+export APPNAME=Kronos
 
 # Deploy dependencies
 quick-sharun /usr/bin/kronos /usr/share/kronos
